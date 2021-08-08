0904ca67f9d2d934310cf6ec5b38a603a0f72bd8
Adrian Cabal <adriancabal@Adrians-MacBook-Pro.local>
2021-08-07 22:33:42 -0500

    added logo and tag line

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import pikaPaymentsLogo from './PikaPaymentsLogo.PNG';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src={pikaPaymentsLogo} className="App-logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p style={{color: 'black'}}>
+          Save mo money
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App; 
