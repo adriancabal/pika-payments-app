08567eee965b9e6152e6375f028320d8cd4bb785
Adrian Cabal <adriancabal@Adrians-MacBook-Pro.local>
2021-08-14 15:52:56 -0500

    we are currently building the site.

--- src/App.js
+++ src/App.js
@@ -7,9 +7,26 @@ function App() {
       <header className="App-header">
         <img src={pikaPaymentsLogo} className="App-logo" />
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p style={{color: 'black'}}>
-          We're still building it!...
-        </p>
+        <h3 style={{ color: 'black' }}>
+          We're currently building the site!...
+        </h3>
+        <div className="text-container">
+          <div>
+            {"Please send us a merchant statement and your company name to "}
+            <span style={{fontWeight: 'bold'}}>
+            <a href={"mailto:contact@pikapayments.com"}>{"contact@pikapayments.com"}</a>
+            {/* {"contact@pikapayments.com"} */}
+            </span>
+            
+            </div>
+          <div>
+            to get a savings estimate on payment processing.
+          </div>
+          
+        </div>
+        <h4 style={{ color: 'black' }}>
+          Thank you!
+        </h4>
       </header>
     </div>
   );
